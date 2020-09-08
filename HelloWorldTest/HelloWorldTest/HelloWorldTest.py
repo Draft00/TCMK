4992318699a326f6fd660dee2f57a04913400c7b
Draft00 <50637382+Draft00@users.noreply.github.com>
2020-09-08 23:36:37 +0300

    add Euclidean algorithm
    with %

--- HelloWorldTest/HelloWorldTest/HelloWorldTest.py
+++ HelloWorldTest/HelloWorldTest/HelloWorldTest.py
@@ -1,2 +1,11 @@
-print("Hello world!")
-    
+def gcd_rem_division(num1, num2):
+    while num1 != 0 and num2 != 0:
+        if num1 >= num2:
+            num1 %= num2
+        else:
+            num2 %= num1
+    print (num1 or num2)
+
+gcd_rem_division(168, 105)
+# print(a) # Выведет 21
+
